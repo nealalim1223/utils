8d9ec0fcefd6c6afda4181fe87ad80a905ecb4b3
nealalim122322 <programmer.neal@gmail.com>
2024-12-27 18:03:18 +0800

    updated

--- controllers/jwt-generate.js
+++ controllers/jwt-generate.js
@@ -2,6 +2,7 @@ const { SignJWT } = require("jose");
 
 module.exports = async (payload, secret, expiresIn) => {
   const jwt = await new SignJWT(payload)
+    .setProtectedHeader({ alg: "HS256" })
     .setExpirationTime(expiresIn)
     .sign(new TextEncoder().encode(secret));
   return jwt;
