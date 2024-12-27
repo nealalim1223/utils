b40c6f28e29e9ec623ee80e5227ef419cd98a902
nealalim122322 <programmer.neal@gmail.com>
2024-12-27 17:08:10 +0800

    updated

--- controllers/jwt-generate.js
+++ controllers/jwt-generate.js
@@ -1,5 +1,8 @@
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
 module.exports = async (payload, secret, options) => {
+  // Ensure options is an object
+  options = (typeof options === 'object' && options !== null) ? options : {};
+
   return jwt.sign(payload, secret, options);
 };
