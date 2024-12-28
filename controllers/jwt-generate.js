8a0742d6ea4484c89e5a6ac26b0e602db31d7208
nealalim122322 <programmer.neal@gmail.com>
2024-12-28 15:27:18 +0800

    updated

--- controllers/jwt-generate.js
+++ controllers/jwt-generate.js
@@ -1,9 +1,36 @@
-const { SignJWT } = require("jose");
+const crypto = require('crypto');
 
-module.exports = async (payload, secret, expiresIn) => {
-  const jwt = await new SignJWT(payload)
-    .setProtectedHeader({ alg: "HS256" })
-    .setExpirationTime(expiresIn)
-    .sign(new TextEncoder().encode(secret));
-  return jwt;
+module.exports = async (payload, secret) => {
+  const header = {
+    alg: "HS256",
+    typ: "JWT"
+  };
+
+  const base64UrlEncode = (obj) => {
+    return Buffer.from(JSON.stringify(obj))
+      .toString('base64')
+      .replace(/=+$/, '')
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_');
+  };
+
+  const encodedHeader = base64UrlEncode(header);
+  const encodedPayload = base64UrlEncode(payload);
+  const signature = (encodedHeader, encodedPayload, secret) => {
+    const data = `${encodedHeader}.${encodedPayload}`;
+    const hmac = crypto.createHmac('sha256', secret);
+    return Buffer.from(hmac.update(data).digest())
+      .toString('base64')
+      .replace(/=+$/, '')
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_');
+  };
+
+  const jwtSignature = signature(encodedHeader, encodedPayload, secret);
+
+  // console.log("Encoded Header:", encodedHeader);
+  // console.log("Encoded Payload:", encodedPayload);
+  // console.log("JWT Signature:", jwtSignature);
+
+  return `${encodedHeader}.${encodedPayload}.${jwtSignature}`;
 };
--- controllers/jwt-verify.js
+++ controllers/jwt-verify.js
@@ -1,10 +1,44 @@
-const { jwtVerify } = require("jose");
+const crypto = require('crypto');
+
+const base64UrlDecode = (str) => {
+  const padding = '='.repeat((4 - str.length % 4) % 4);
+  return JSON.parse(Buffer.from((str + padding).replace(/-/g, '+').replace(/_/g, '/'), 'base64').toString());
+};
+
+const base64UrlEncode = (obj) => {
+  return Buffer.from(obj)
+    .toString('base64')
+    .replace(/=+$/, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_');
+};
+
+const signatureFunction = (header, payload, secret) => {
+  const data = `${header}.${payload}`;
+  const hmac = crypto.createHmac('sha256', secret);
+  return base64UrlEncode(hmac.update(data).digest());
+};
 
 module.exports = async (token, secretKey) => {
-  try {
-    const { payload } = await jwtVerify(token, new TextEncoder().encode(secretKey));
-    return payload;
-  } catch (error) {
+  if (typeof token !== 'string') {
+    throw new TypeError("Token must be a string");
+  }
+
+  // console.log("Received Token:", token);
+
+  const [header, payload, signature] = token.split('.');
+
+  // console.log("Header:", header);
+  // console.log("Payload:", payload);
+
+  const expectedSignature = signatureFunction(header, payload, secretKey);
+  // console.log("Expected Signature:", expectedSignature);
+  // console.log("Provided Signature:", signature);
+
+  if (expectedSignature !== signature) {
     throw new Error("Invalid token");
   }
-}
+
+  const decodedPayload = base64UrlDecode(payload);
+  return decodedPayload;
+};
